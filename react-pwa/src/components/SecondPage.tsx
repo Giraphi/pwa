09ef652e280b4850aefbcfa1b1f589d44f3dea3b
Giraphi <hoeps.raphael@gmail.com>
2023-01-12 12:14:40 +0100

    PWA base setup

--- react-pwa/src/components/SecondPage.tsx
+++ react-pwa/src/components/SecondPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Page from "./style-components/Page";
 import Windows from "../img/windows.webp";
 import { StyledImage, StyledLink } from "./style-components/StyledImage";
@@ -6,8 +6,10 @@ import { StyledImage, StyledLink } from "./style-components/StyledImage";
 export interface SecondPageProps {}
 
 export default function SecondPage(props: SecondPageProps) {
+    const [ctr, setCtr] = useState(0);
     return (
         <Page>
+            <h3 onClick={() => setCtr((x) => x + 1)}>Counter: {ctr}</h3>
             <StyledImage src={Windows} />
             <h1>Second Page</h1>
             <StyledLink to={"/"}>Back Home</StyledLink>
