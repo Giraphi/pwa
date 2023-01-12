cd71a4d575a38a33bc513ef57df5a59696deb9e2
Giraphi <hoeps.raphael@gmail.com>
2023-01-12 12:08:42 +0100

    PWA base setup

--- react-pwa/src/components/style-components/StyledImage.tsx
+++ react-pwa/src/components/style-components/StyledImage.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const StyledImage = styled.img`
-    height: 50vh;
+    max-height: 50vh;
+    max-width: 90vw;
 `;
 
 export const StyledLink = styled(Link)`
